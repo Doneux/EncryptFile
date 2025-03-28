a13275799b5bb2c4e52674f66c1149faf0327a98
Doneux <85197474+Doneux@users.noreply.github.com>
2025-03-28 10:23:02 -0700

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,24 +1,30 @@
-print("Please enter a filename.\nInput example: test.dec")
+def dec():
+    print("Please enter a filename.\nInput example: test.dec")
 
-while 1:
-    filename = input("Enter filename: ")
-    if (".dec" not in filename) or (" " in filename):
-        print("Error: Invalid Input")
-    else:
-        try:
-            file = open(filename, "r")
-            break
-        except:
-            print("Error: Cannot open file")
-    break
-        
-#find the file extension in filename
-perIndex = filename.rfind('.')
-#add dec, then save as txt
-newname = filename[:perIndex] + '_dec.txt'
+    while 1:
+        filename = input("Enter filename: ")
+        if (".dec" not in filename) or (" " in filename):
+            print("Error: Invalid Input")
+        else:
+            try:
+                file = open(filename, "r")
+                break
+            except:
+                print("Error: Cannot open file")
+        break
+            
+    #find the file extension in filename
+    perIndex = filename.rfind('.')
+    #add dec, then save as txt
+    newname = filename[:perIndex] + '_dec.txt'
 
-savefile = open(newname, "w")
+    savefile = open(newname, "w")
 
-for i in file:
-    for j in i:
-        savefile.write(chr(int(((ord(j)) / 2))))
+    for i in file:
+        for j in i:
+            #ugly, temporary decryption, dividing the integer value of each char byte by 2
+            #TODO: replace
+            savefile.write(chr(int(((ord(j)) / 2))))
+
+if __name__ == '__main__':
+    dec()
