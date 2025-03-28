4f065f52d7e8f2b597241eaa25ef0ed0c1e7aca0
Doneux <85197474+Doneux@users.noreply.github.com>
2025-03-27 23:42:22 -0700

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,24 @@
+print("Please enter a filename.\nInput example: test.dec")
+
+while 1:
+    filename = input("Enter filename: ")
+    if (".dec" not in filename) or (" " in filename):
+        print("Error: Invalid Input")
+    else:
+        try:
+            file = open(filename, "r")
+            break
+        except:
+            print("Error: Cannot open file")
+    break
+        
+#find the file extension in filename
+perIndex = filename.rfind('.')
+#add dec, then save as txt
+newname = filename[:perIndex] + '_dec.txt'
+
+savefile = open(newname, "w")
+
+for i in file:
+    savefile.write(chr(ord(i) / 2))
+
