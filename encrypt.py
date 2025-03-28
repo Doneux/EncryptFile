3240ef500c57170ec57a5322ee7a540450421bad
Doneux <85197474+Doneux@users.noreply.github.com>
2025-03-27 23:36:28 -0700

    Create encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,24 @@
+print("Please enter a filename.\nInput example: test.txt")
+
+while 1:
+    filename = input("Enter filename: ")
+    if ("." not in filename) or (" " in filename):
+        print("Error: Invalid Input")
+    else:
+        try:
+            file = open(filename, "r")
+            break
+        except:
+            print("Error: Cannot open file")
+    break
+        
+#find the file extension in filename
+perIndex = filename.rfind('.')
+#replace it with .dec, which our decryption will read
+newname = filename[:perIndex] +'.dec'
+
+savefile = open(newname, "w")
+
+for i in file:
+    savefile.write(i)
+
