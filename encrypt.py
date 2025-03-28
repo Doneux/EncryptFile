1dd583616c680f634a1f15a9129fa5aa7f590fe0
Doneux <85197474+Doneux@users.noreply.github.com>
2025-03-28 10:24:28 -0700

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,25 +1,30 @@
+def enc():
+    print("Please enter a filename.\nInput example: test.txt")
+    
+    while 1:
+        filename = input("Enter filename: ")
+        if ("." not in filename) or (" " in filename):
+            print("Error: Invalid Input")
+        else:
+            try:
+                file = open(filename, "r")
+                break
+            except:
+                print("Error: Cannot open file")
+        break
+            
+    #find the file extension in filename
+    perIndex = filename.rfind('.')
+    #replace it with .dec, which our decryption will read
+    newname = filename[:perIndex] +'.dec'
+    
+    savefile = open(newname, "w")
+    
+    for i in file:
+        for j in i:
+            #ugly, temporary encryption, multiplying the integer value of each char bytes by 2
+            #TODO: replace
+            savefile.write((chr(ord(j) * 2)))
 
-print("Please enter a filename.\nInput example: test.txt")
-
-while 1:
-    filename = input("Enter filename: ")
-    if ("." not in filename) or (" " in filename):
-        print("Error: Invalid Input")
-    else:
-        try:
-            file = open(filename, "r")
-            break
-        except:
-            print("Error: Cannot open file")
-    break
-        
-#find the file extension in filename
-perIndex = filename.rfind('.')
-#replace it with .dec, which our decryption will read
-newname = filename[:perIndex] +'.dec'
-
-savefile = open(newname, "w")
-
-for i in file:
-    for j in i:
-        savefile.write((chr(ord(j) * 2)))
+if __name__ == '__main__':
+    enc()
